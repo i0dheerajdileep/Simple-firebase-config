e24b77575c146775d330c4f7c4612edf8e79017c
Dheeraj dileep <94816195+i0dheerajdileep@users.noreply.github.com>
2023-04-06 16:27:31 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 const firebaseConfig = {
-    apiKey: "AIzaSyC96ot-qWryLjAFfVlRKzb3_NMOopjVBwo",
-    authDomain: "fir-ddd69.firebaseapp.com",
-    projectId: "fir-ddd69",
-    storageBucket: "fir-ddd69.appspot.com",
-    messagingSenderId: "1002813325983",
-    appId: "1:1002813325983:web:2982f7a3bdc5a03b7458aa",
-    measurementId: "G-K6VPTQ15PL"
-  };
-  
+  apiKey: "************",
+  authDomain: "************",
+  projectId: "************",
+  storageBucket: "************",
+  messagingSenderId: "************",
+  appId: "************",
+  measurementId: "************"
+};
   const app = initializeApp(firebaseConfig)
   const db = getFirestore(app)
   export { db }
